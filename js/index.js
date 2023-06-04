5ec98a859abcedc1058a8c9479db137fa31f70fb
Congyes11 <126838496+Congyes11@users.noreply.github.com>
2023-06-05 05:35:54 +1000

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function divShow() {
+	let mobile = document.getElementById("navs")
+	if(mobile.style.display = 'none'){
+		mobile.style.display = 'block'
+	}
+}
+function closed() {
+	let mobile = document.getElementById("navs")
+	if(mobile.style.display = 'block'){
+		mobile.style.display = 'none'
+	}
+}
+function meuntc(){
+	let mobile = document.getElementById("tc")
+	if(mobile.style.display = 'none'){
+		mobile.style.display = 'block'
+	}
+}
+function tclose(){
+	let mobile = document.getElementById("tc")
+	if(mobile.style.display = 'block'){
+		mobile.style.display = 'none'
+	}
+}
